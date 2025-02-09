cf3cdd786166ab6423beb3e5479ac77e56acbf71
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2025-02-09 19:04:28 -0300

    imports

--- tutorials/server.js
+++ tutorials/server.js
@@ -1,1 +1,10 @@
 console.log('Hello world from node!');
+
+
+// CommonJS imports
+const os = require('os');
+console.log(os.type());
+
+const { add, subtract, multiply, divide } = require('./utils/math');
+
+console.log(add(3, 2));
